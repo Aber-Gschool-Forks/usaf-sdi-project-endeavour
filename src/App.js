618913cfc8b16aa47a9857f377157b1775d2cce7
Eric Eash <ericeash@gmail.com>
2021-04-07 08:45:18 -0700

    Deleted something

--- src/App.js
+++ src/App.js
@@ -5,10 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          .
-        </p>
-          Beer
+        <p></p>
         </a>
       </header>
     </div>
