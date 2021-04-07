75719882fdef0b3947c146c57facb685b98fbd7d
Ethan <ethan.e.wise@gmail.com>
2021-04-07 10:01:16 -0700

    brewerypage WIP

--- src/BreweryContext.js
+++ src/BreweryContext.js
@@ -1,5 +1,6 @@
 import { createContext } from "react";
 
 const LocationContext = createContext();
+const BreweryContext = createContext();
 
-export default LocationContext;
+export { LocationContext, BreweryContext };
