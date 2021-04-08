d5dff12805436d75f856804310f5e0bafcddc162
Eric Eash <ericeash@gmail.com>
2021-04-07 22:31:39 -0700

    a little bit of css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
+import '../styles/Header.css';
+
 export default function Header() {
   return (
-    <nav>
+    <nav className="header">
       <section className="BrewListLogo">
-        <div>Here is a Brew List Logo</div>
+      <iframe className="brewLogo1" src="https://giphy.com/embed/cNDlnilnnPjOAIZsxl" width="150" height="150" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
       </section>
-      <section className="BrewListTitle">Brew List</section>
+      <img className="brewTitle" src="https://fontmeme.com/permalink/210408/47e59bd02bee6b480cf6b5f8a4fa4f59.png" alt="cool-fonts" border="0"></img>
     </nav>
   );
 }
